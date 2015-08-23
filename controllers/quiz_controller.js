9a319a1942d4e80439e55e428b9ce5dc314d69f9
gutierrez8006 <gutierrez.8006@gmail.com>
2015-08-23 12:01:26 -0500

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,3 +31,21 @@ exports.answer = function(req, res){
 	}
 	res.render('layout', {quiz: req.quiz, respuesta: resultado, body: 'answer'});
 };
+
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(// crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('layout', {quiz: quiz, body: 'new'});
+};
+
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	console.log('ingreso a contollador create');
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}); // Redireccion HTTP (URL relativo) lista de preguntas
+};
